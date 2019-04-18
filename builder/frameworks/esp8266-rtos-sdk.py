0069405ebbd2ed94b9c052024cb880bd4c555688
WallaceWilliam <volos4@ukr.net>
2019-04-18 19:40:27 +0300

    modified:   builder/frameworks/esp8266-rtos-sdk.py

--- builder/frameworks/esp8266-rtos-sdk.py
+++ builder/frameworks/esp8266-rtos-sdk.py
@@ -689,7 +689,7 @@ build_dirs = [
     "libsodium", "mqtt",
     "aws_iot",  
     "esp_https_ota", "protocomm", "wifi_provisioning",
-    "esp_https_server", "esp_ringbuf", "console",
+    "esp_https_server", "esp_ringbuf", "console", "spi_ram",
 ]
 build_excl = [
     "bootloader", "esptool_py", "partition_table",
